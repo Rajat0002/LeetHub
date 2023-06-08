c76548777703a6db652c9d9491f6f578ae50b2d3
Rajat <rajatbassi002@gmail.com>
2023-06-08 09:44:52 +0530

    _

--- script.py
+++ script.py
@@ -1,41 +1,41 @@
-# import subprocess
-
-# tag_name = "v1.0.0"
-# commit_hash = "HEAD"
-# tag_message = "Initial release"
-
-
-# command = f"git tag -a {tag_name} {commit_hash} -m \"{tag_message}\""
-# subprocess.run(command, shell=True, check=True)
-
-# # Push the tag to the remote repository
-# push_command = f"git push origin {tag_name}"
-# subprocess.run(push_command, shell=True, check=True)
-
-
-
 import subprocess
 
-# Get the latest tag
-last_tag = subprocess.check_output(['git', 'describe', '--abbrev=0', '--tags']).strip().decode()
-
-# Split the tag into major, minor, and patch components
-major, minor, patch = last_tag.split(".")
-
-# Increment the major component
-new_major = int(major) + 1
-
-# Construct the new tag
-new_tag = f"{new_major}.0.0"
-
-# Define the commit hash and tag message
+tag_name = "v2.0.0"
 commit_hash = "HEAD"
-tag_message = "New release"
+tag_message = "Initial release"
 
-# Create the tag
-command = f"git tag -a {new_tag} {commit_hash} -m \"{tag_message}\""
+
+command = f"git tag -a {tag_name} {commit_hash} -m \"{tag_message}\""
 subprocess.run(command, shell=True, check=True)
 
 # Push the tag to the remote repository
-push_command = f"git push origin {new_tag}"
+push_command = f"git push origin {tag_name}"
 subprocess.run(push_command, shell=True, check=True)
+
+
+
+# import subprocess
+
+# # Get the latest tag
+# last_tag = subprocess.check_output(['git', 'describe', '--abbrev=0', '--tags']).strip().decode()
+
+# # Split the tag into major, minor, and patch components
+# major, minor, patch = last_tag.split(".")
+
+# # Increment the major component
+# new_major = int(major) + 1
+
+# # Construct the new tag
+# new_tag = f"{new_major}.0.0"
+
+# # Define the commit hash and tag message
+# commit_hash = "HEAD"
+# tag_message = "New release"
+
+# # Create the tag
+# command = f"git tag -a {new_tag} {commit_hash} -m \"{tag_message}\""
+# subprocess.run(command, shell=True, check=True)
+
+# # Push the tag to the remote repository
+# push_command = f"git push origin {new_tag}"
+# subprocess.run(push_command, shell=True, check=True)
